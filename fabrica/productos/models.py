6348269a3e7d610414ad3038015c9db58c6b430b
SrWoonejos <danmiranda@gmail.com>
2025-02-01 20:50:57 -0300

    update desafio

--- fabrica/productos/models.py
+++ fabrica/productos/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Manufacturer(models.Model):
     name = models.CharField(max_length=100)
+    pais  = models.CharField(max_length=100, blank=True, default='')
 
     def __str__(self):
         return self.name
